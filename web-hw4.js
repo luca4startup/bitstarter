16936d4134ec5e8c4837e51c4f7a3944d4d28a2e
Ubuntu <ubuntu@ip-172-31-38-117.us-west-2.compute.internal>
2013-07-18 17:05:34 +0000

    hw4 - new web.js and index.html

---
+++ web-hw4.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var fs = require('fs'); // added for fs read write
+
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  // response.send('Hello World 2!');
+  var buf = new Buffer (1024) ; // confirm size 
+  buf = fs.readFileSync ('index.html') ;
+  response.send(buf.toString()) ;
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
